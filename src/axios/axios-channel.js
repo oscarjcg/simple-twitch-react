7755a55d6618607f9cc844c8f1d69d29a5814716
Oscar Catari Gutierrez <oscarcatari@outlook.es>
2022-11-03 19:56:38 +0000

    [fix] Prepare for new backend

--- src/axios/axios-channel.js
+++ src/axios/axios-channel.js
@@ -1,8 +1,8 @@
 import axios  from 'axios';
 
 const instance = axios.create({
-    //baseURL: 'https://localhost:7109' 
-    baseURL: 'https://backend-simple-twitch.oscarcatarigutierrez.com/api/'
+    //baseURL: 'http://localhost:8086/' 
+    baseURL: 'https://st-dotnet.oscarcatarigutierrez.com/'
 });
 
 export default instance;
